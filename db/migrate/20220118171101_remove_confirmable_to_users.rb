63d25002c86f1976cedc276116cd74583b0755bf
Natblow <natblow@hotmail.fr>
2022-04-08 11:57:43 -0400

    update

--- db/migrate/20220118171101_remove_confirmable_to_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveConfirmableToUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :confirmation_token, :string
-    remove_column :users, :confirmed_at, :datetime
-    remove_column :users, :confirmed_sent_at, :datetime
-    remove_column :users, :unconfirmed_email, :string
-  end
-end
